f32f0cc1afcd8a3844eb226da6c9b4898e8e0d72
paarthvisharma <sharma.paa@northeastern.edu>
2024-02-08 23:49:22 -0500

    Activity 8 Feb

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
-
+import Gallery from './qcomps/props_item'
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Gallery />
     </div>
   )
 }
--- src/pages/qcomps/props_item.js
+++ src/pages/qcomps/props_item.js
@@ -2,6 +2,9 @@ function Item({ name, isPacked }) {
   if (isPacked) {
     return <li className="item">{name} ✔</li>;
   }
+  if (!isPacked){
+    return  <li className="item"> <del> {name} </del>  </li>;
+  }
   return <li className="item"> {name} </li>
 }
 
